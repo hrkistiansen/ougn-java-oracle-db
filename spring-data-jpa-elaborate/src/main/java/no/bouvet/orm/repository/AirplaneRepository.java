cf60257ae6fbc357d6a0810c117acfbd76ca1f07
havard.kristiansen <havard.kristiansen@bouvet.no>
2015-03-10 10:04:12 +0100

    Removed obsolete project

--- spring-data-jpa-elaborate/src/main/java/no/bouvet/orm/repository/AirplaneRepository.java
+++
@@ -1,9 +0,0 @@
-package no.bouvet.orm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import no.bouvet.orm.domain.Airplane;
-
-public interface AirplaneRepository extends CrudRepository<Airplane, Long>{
-
-}
